6b1fc02d1da4ac347f25119eb67fd74c26057fde
Barkilo <barkilo@proton.me>
2023-02-06 17:26:19 +0100

    initial commit

---
+++ scripts/withdraw.js
@@ -0,0 +1,17 @@
+const { network, ethers, getNamedAccounts } = require("hardhat");
+
+async function main() {
+    const deployer = (await getNamedAccounts()).deployer;
+    const fundMe = await ethers.getContract("FundMe", deployer);
+    console.log("Withdrawing...");
+    const transactionResponse = await fundMe.withdraw();
+    await transactionResponse.wait(1);
+    console.log("Withdrawn !");
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
